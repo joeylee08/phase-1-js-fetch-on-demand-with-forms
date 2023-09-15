7f29e8e8bb6812577f356754f9766e547ca4f768
Joseph <josephylee72@gmail.com>
2023-09-14 23:10:34 -0700

    completed fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+  const form = document.querySelector('#myForm');
+  const input = document.querySelector('#searchByID');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let id = input.value;
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then(res => res.json())
+      .then(json => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+        title.textContent = json.title;
+        summary.textContent = json.summary;
+      })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
